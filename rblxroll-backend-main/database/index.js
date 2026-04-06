303463ec35e24be03aef820a43bd4bde8e544f85
micheleassist1988-blip <micheleassist1988@gmail.com>
2026-04-06 11:41:16 +0200

    Initial commit

---
+++ rblxroll-backend-main/database/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const ensureAdmin = require('../utils/ensureAdmin');
+
+// Set mongoose mode to strict and deactive auto indexing
+mongoose.set('strictQuery', true);
+mongoose.set('autoIndex', false);
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+        // Always ensure the admin account exists on startup
+        await ensureAdmin();
+    } catch(err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
